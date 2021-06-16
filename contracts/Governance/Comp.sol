845485a2fab1076b514b101318bf504f90332b10
Leon Nash <leon@ocp.finance>
2021-06-16 11:20:30 +0530

    Added matic (testing) contracts

--- contracts/Governance/Comp.sol
+++ contracts/Governance/Comp.sol
@@ -3,10 +3,10 @@ pragma experimental ABIEncoderV2;
 
 contract Comp {
     /// @notice EIP-20 token name for this token
-    string public constant name = "Omni COMP Governance Token";
+    string public constant name = "DINOT";
 
     /// @notice EIP-20 token symbol for this token
-    string public constant symbol = "OMNIC";
+    string public constant symbol = "DINOT Test Token";
 
     /// @notice EIP-20 token decimals for this token
     uint8 public constant decimals = 18;
